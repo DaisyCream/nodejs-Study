97fd942dbebbdd3e6c8c50230d11c2691873cd3f
DaisyCream@qq.com <DaisyCream@qq.com>
2015-12-14 00:01:05 +0800

    add the http

---
+++ http/http.js
@@ -0,0 +1,36 @@
+var http = require('http');
+
+http.createServer(function(request,response){
+    response.writeHead(200, { 'Content-Type': 'text-plain'});
+    response.end('Hello World\n');
+}).listen(8124);
+
+/*********************将request当做只读的数据流*********************************/
+http.createServer(function(request,response){
+    var body = {};
+    console.log(request.method);
+    console.log(request.headers);
+
+    request.on('data', function(chunk){
+        body.push(chunk);
+    });
+
+    request.on('end', function(){
+        body = Buffer.concat(body);
+        console.log(body.toString());
+    });
+}).listen(80);
+
+
+/*********************将response当做只写入的数据流来写入*********************************/
+http.createServer(function(request,response){
+    response.writeHead(200, { 'Content-Type' : 'text/plain' });
+
+    request.on('data', function(chunk){
+        response.write(chunk);
+    });
+
+    request.on('end', function(){
+        response.end();
+    })
+}).listen(80);
--- nodeTest/hello.js
+++ nodeTest/hello.js
@@ -1,20 +1,45 @@
 var EventEmitter = require('events').EventEmitter;
+//
+//var util = require('util');
+//
+//
+//var Promise = function(){
+//    EventEmitter.call(this);
+//};
+//
+//util.inherits(Promise,EventEmitter);
+//
+//Promise.prototype.then = function(fulfilledHandler,errorHandler,progressHandler){
+//    if(typeof fulfilledHandler === 'function'){
+//        this.once('success', fulfilledHandler);
+//    }
+//    if(typeof errorHandler === 'function'){
+//        this.once('error', errorHandler);
+//    }
+//    if(typeof progressHandler === 'function'){
+//        this.on('progress',progressHandler);
+//    }
+//};
+//
+//
+//var Deferred = function(){
+//    this.state = 'unfulfilled';
+//    this.promise = new Promise();
+//};
+//
+//Deferred.prototype.resolve = function(obj){
+//    this.state = 'fulfilled';
+//    this.promise.emit("success",obj);
+//};
+//
+//Deferred.prototype.reject = function(err){
+//    this.state = 'failed';
+//    this.promise.emit('error', err);
+//};
+//
+//Deferred.prototype.progress = function(data){
+//    this.promise.emit('progress', data);
+//};
 
-var Promise = function(){
-    EventEmitter.call(this);
-};
-util.inherits(Promise,EventEmitter);
 
-
-Promise.prototype.then = function(fulfilledHandler,errorHandler,progressHandler){
-    if(typeof fulfilledHandler === 'function'){
-        this.once('success', fulfilledHandler);
-    }
-    if(typeof errorHandler === 'function'){
-        this.once('error', errorHandler);
-    }
-    if(typeof progressHandler === 'function'){
-        this.on('progress',progressHandler);
-    }
-};
 
