1e74ad1513e47ad2d33fc556b41808fc984c4dd4
DaisyCream@qq.com <DaisyCream@qq.com>
2015-12-15 23:19:45 +0800

    add the connet.js

--- http/socketTest.js
+++ http/socketTest.js
@@ -6,6 +6,28 @@ net.createServer(function (conn) {
             'Content-Length: 11',
             '',
             'Hello World'
-        ]).join('\n');
+        ].join('\n'));
     });
 }).listen(80);
+
+var options = {
+    port: 80,
+    host: 'www.example.com'
+};
+
+var client = net.connect(options,function(){
+    client.write([
+        'GET /HTTP/1.1',
+        'User-Agent: curl/7.26/0',
+        'Host: www.baidu.com',
+        'Accept : */*',
+        '',
+        ''
+    ].join('\n'));
+});
+
+
+client.on('data',function(data){
+    console.log(data.toString());
+    client.end();
+});
