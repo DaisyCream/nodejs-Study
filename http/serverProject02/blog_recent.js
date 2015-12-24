ab07eb038ae78975556c1842332b8055ef61a873
DaisyCream@qq.com <DaisyCream@qq.com>
2015-12-24 19:18:05 +0800

    add the serverProjecr02

---
+++ http/serverProject02/blog_recent.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var fs = require('fs');
+
+//http.createServer(function(req, res){
+//    if(req.url == '/'){
+//        fs.readFile('./titles.json',function(err, data){
+//            if(err){
+//                console.log(err);
+//                res.end(err);
+//            }else{
+//                var titles = JSON.parse(data.toString());
+//
+//                fs.readFile('./template.html',function(err, data){
+//                    if(err){
+//                        console.log(err);
+//                        res.end(err);
+//                    }else{
+//                        var tmpl = data.toString();
+//
+//                        var html = tmpl.replace('%', titles.join('</li><li>'));
+//                        res.writeHead(200,{
+//                            'Content-Type' : text/html
+//                        });
+//                        res.end(html);
+//                    }
+//                });
+//            }
+//        });
+//    }
+//
+//}).listen(8000, "127.0.0.1");
+
+
+http.createServer(function(req, res){
+    getTitles(res);
+}).listen(8000, '127.0.0.1');
+
+function getTitles(res){
+    fs.readFile('./titles.json', function(err, data){
+       if(err)
+           return handleErr(err,res);
+        getTemplate(JSON.parse(data.toString()), res);
+    });
+}
+
+
+function getTemplate(data, res){
+    fs.readFile('./template.html',function(err, temp){
+        if(err){
+            handleErr(err);
+        }else{
+            formatHtml(data,temp,res);
+        }
+    })
+}
+
+function formatHtml(data, temp, res){
+    var html = temp.replace('&',data.join('</li><li>'));
+    res.writeHead(200,{
+        'Content-Type': 'text/html'
+    });
+    res.end(html);
+}
+
+function handleErr(err, res){
+    console.log(err);
+    res.end(err);
+}
