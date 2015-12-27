c7d0227f1525c5c806e925c22d8d6f25fbf8606b
DaisyCream@qq.com <DaisyCream@qq.com>
2015-12-27 14:50:08 +0800

    add the watcher.js

--- http/serverProject04/watcher.js
+++ http/serverProject04/watcher.js
@@ -1,60 +1,48 @@
-
-
 /*****************inherits the eventEmitter**********************/
-
+var fs = require('fs');
 var events = require('events');
 var util = require('util');
 
-/**
- * create a class named watcher
- * @param watchDir
- * @param processDir
- */
-
 function watcher(watchDir, processDir){
-    this.watchDir = watchDir; //listen the dir
-    this.processDir = processDir;   //put in dir
+    this.watchDir = watchDir;
+    this.processDir = processDir;
 }
 
+
 util.inherits(watcher, events.EventEmitter);
-
-
-/*****************event emitter**********************/
-
-var fs = require('fs');
-var watchDir = './watch';
-var processDir = './done';
+//watcher.prototype = new events.EventEmitter();
 
 watcher.prototype.watch = function(){
-   var watcher = this;
-   fs.readdir(this.watchDir, function(err, files){
-       if(err) throw err;
-       for(var index in files){
-           watcher.emit('process', files[index]);
-       }
-   });
+    var watcher = this;
+    fs.readdir(this.watchDir,function(err, files){
+        if(err) throw err;
+        for(var index in files){
+            watcher.emit('process',files[index]);
+        }
+    });
 };
-
 
 watcher.prototype.start = function(){
     var watcher = this;
-    fs.readFile(watchDir, function(){
+    fs.watchFile(this.watchDir, function(){
         watcher.watch();
     });
 };
 
-/***************************************************/
+
+/*********************************************/
+var watchDir = './watch';
+var processDir = './done';
+
 var watchers = new watcher(watchDir, processDir);
 
-watchers.on('process', function(file){
+watchers.on('process' ,function(file){
     var watchFile = this.watchDir + '/' + file;
     var processFile = this.processDir + '/' + file.toLowerCase();
 
-    fs.rename(watchFile,processFile,function(err){
+    fs.rename(watchFile, processFile, function(err){
         if(err) throw err;
     });
+
 });
-
-watchers.start();
-
 
