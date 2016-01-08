915c39eb32ae60c9ec5422ddc03e0b66e5d3eea0
DaisyCream@qq.com <DaisyCream@qq.com>
2016-01-08 23:38:10 +0800

    know the to_do_list.js

--- server/to-do-list/to-do-list.js
+++ server/to-do-list/to-do-list.js
@@ -1,29 +1,16 @@
 var http = require('http');
+var qs = require('querystring');
 
 var list = [];
-
-var html = '<html><head>' +
-    '<title>ToDoList</title>' +
-    '</head>' +
-    '<body>' +
-    '<h1>ToDoList</h1>' +
-    '<div id="list"></div>' +
-    '<form action="/" method="post">' +
-    '<input type="text">' +
-    '<input type="submit">' +
-    '</from>' +
-    '</body></html>';
-
-
 
 var server = http.createServer(function(req, res){
     if(req.url == '/'){
         switch(req.method){
             case 'GET':
-                res.end(html);
+                sendData(res);
                 break;
             case 'POST':
-
+                getData(req, res);
                 break;
             default :
                 console.log('Request method is error');
@@ -33,14 +20,63 @@ var server = http.createServer(function(req, res){
     }
 });
 
+function sendData(res){
+    var html = '<html><head><meta charset="UTF-8">' +
+        '<title>ToDoList</title>' +
+        '</head>' +
+        '<body>' +
+        '<h1>ToDoList</h1>' +
+        '<ul>' +
+         list.forEach(function(item){
+             return '<ul>' + item + '</ul>';
+         })+
+        '</ul>'+
+        '<form action="/" method="post">' +
+        '<input type="text" name="listThing">' +
+        '<input type="submit" value="提交">' +
+        '</from>' +
+        '</body></html>';
 
-function getData(req){
+    //var  html = '<html><head><title>Todo List</title></head><body>' +
+    //    '<h1>Todo List</h1>' +
+    //    '<ul>' +
+    //    list.map(function(item){
+    //        return '<li>'+ item +'</li>';
+    //    }).join('') +
+    //    '</ul>' +
+    //    '<form method="post" action="/">' +
+    //    '<p><input type="text" name = "item"></p>' +
+    //    '<p><input type="submit" value="Add Item"></p>' +
+    //    '</form></body></html>';
+
+    res.writeHead('Content-Type', 'text/html');
+    res.writeHead('Content-Length', Buffer.byteLength(html));
+    res.end(html);
+}
+
+
+function getData(req, res){
     var body = '';
-    req.on('data', function(data){
-        body+=data;
+    req.setEncoding('utf8');
+    //console.log(req.method);
+    req.on('data', function(chunk){
+        body += chunk;
+    });
+    req.on('end', function(){
+        //console.log(typeof body);
+        var obj = qs.parse(body);
+        console.log(obj);
+        list.push(obj.listThing);
+        sendData(res);
     });
 }
 
 
 server.listen(3000);
 console.log('Server running at 127.0.0.1:3000/');
+
+
+
+
+
+
