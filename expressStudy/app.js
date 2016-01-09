16dac9e9b952e55803eaee42574c73e3aa32ca7c
DaisyCream@qq.com <DaisyCream@qq.com>
2016-01-10 01:09:55 +0800

    study the moudles EXPRESS

--- expressStudy/app.js
+++ expressStudy/app.js
@@ -23,3 +23,49 @@ app.all('/secret', function(req, res){
     next();
 });
 
+
+
+
+
+/*******************路由回调**************************/
+app.get('example/b', function(req, res, next){//用多个回调函数处理路由（记得指定next对象）
+    console.log('1');
+    next();
+},function(req, res){
+    res.send('Hello from B!')
+});
+
+
+/*********************************************/
+
+//使用回调函数数组处理路由
+
+var cb0 = function(req, res, next){
+    console.log('CB0');
+    next();
+};
+
+var cb1 = function(req, res, next){
+    console.log('CB1');
+    next();
+};
+
+var cb2 = function(req, res){
+    res.send('Hello form C!');
+};
+
+app.get('/example', [cb0,cb1,cb2]);
+
+
+/*******************利用Express托管静态文件*******************/
+//假设在 public 目录放置了图片、CSS 和 JavaScript 文件
+
+app.use(express.static('public'));
+
+
+//如果你希望所有通过 express.static 访问的文件都存放在
+// 一个“虚拟（virtual）”目录（即目录根本不存在）下面，可
+// 以通过为静态资源目录指定一个挂载路径的方式来实现，如下所示：
+
+app.use('/static', express.static('public'));
+
