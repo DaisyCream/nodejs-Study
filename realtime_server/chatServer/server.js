10b198956774750a5df41bc6b636123ba124aedf
DaisyCream@qq.com <DaisyCream@qq.com>
2016-01-10 14:40:41 +0800

    add the study/socketIoStudy

--- realtime_server/chatServer/server.js
+++ realtime_server/chatServer/server.js
@@ -1,5 +1,48 @@
 var http = require('http');
 var fs = require('fs');
-var express = require('express');
+var mime = require('mime');
+var path = require('path');
+var cache = {};
 
-express.use();
+var server = http.createServer(function(req, res){
+    var path = '';
+    if(req.url == '/'){
+        path = 'public/index.js';
+    }else{
+        path = 'public/' + req.url;
+    }
+
+    var absPath = './' + path;
+    fs.exists(absPath,function(exsist){
+        if(!exsist){
+            send404(res);
+            return;
+        }
+        res.writeHead(200, {
+            'Content-Type' : mime.lookup(path.basename(absPath))
+        });
+        getData(absPath, cache, res);
+    });
+
+});
+
+function send404(res){
+    res.statusCode = 404;
+    res.end("Can't find the file");
+}
+
+function getData(absPath, cache, res){
+    var stream = fs.createReadStream(absPath);
+    if(cache[absPath]){
+        res.end(cache[absPath]);
+    }else{
+        stream.pipe(cache[absPath]);
+        stream.pipe(res);
+    }
+}
+
+server.listen(3000,function(){
+    console.log('Server running at 127.0.0.1:3000/');
+});
+
+
---
+++ study/socketIoStudy/test01/test01.js
@@ -0,0 +1,22 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+var html = fs.readFileSync('index.html', 'utf-8');
+
+
+function handler(req, res){
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content.Length', Buffer.byteLength(html, 'utf8'));
+    res.end(html);
+}
+
+function tick(){
+    var now = new Date().toUTCString();
+    io.sockets.send(now);
+}
+
+setInterval(tick, 1000);
+
+app.listen(8888,function(){
+    console.log('Server running at 127.0.0.1:8888/');
+});
