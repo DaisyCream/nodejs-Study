7a83eaf103ae6aaf12e66907196c5acba0b1598a
DaisyCream@qq.com <DaisyCream@qq.com>
2016-02-13 11:21:21 +0800

    add the formidable review

--- test1/updateBigFile/server.js
+++ test1/updateBigFile/server.js
@@ -12,28 +12,44 @@ var root = __dirname;
 var server = http.createServer(function(req, res){
     var reqPath = url.parse(req.url).pathname;
     var filePath = false;
-    switch(req.method){
-         case 'GET':
-             if(reqPath == '/'){
-                 filePath = path.join(root, '/index.html');
-                 sendHtml(filePath, res);
-             }
-             break;
-         case 'POST':
-             upLoad(res);
-             break;
-     }
+    if(reqPath == '/'){
+        switch(req.method){
+            case 'GET':
+                filePath = path.join(root, '/index.html');
+                sendHtml(filePath, res);
+                break;
+            case 'POST':
+                upLoad(req, res);
+                break;
+        }
+    }
 });
 
 
 function sendHtml(filePath, res){
     var stream = fs.createReadStream(filePath);
-    res.writeHead('Content-Type', 'text/html');
+    res.writeHead(200,{'Content-Type': 'text/html'});
     stream.pipe(res);
 }
 
-function upLoad(res){
-    var form = formidable();
+function upLoad(req, res){
+    if(!isFormidable(req)){
+        res.statusCode = 400;
+        res.end('Bad request : exception multipart/form-data');
+        return;
+    }
+
+    var form = new formidable.IncomingForm();
+    form.uploadDir = '/tmp';
+    form.parse(req, function(err, fileds, files){
+        fs.rename(files.file.path,'./img/'+fileds.filed + '.png');
+    });
+
+}
+
+function isFormidable(req){
+    var type = req.headers['content-type'] || '';
+    return 0 == type.indexOf('multipart/form-data');
 }
 
 server.listen(3000, function(){
