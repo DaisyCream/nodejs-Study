7d05619ccb199eba00b8c8511444f370f1ea2be0
DaisyCream@qq.com <DaisyCream@qq.com>
2016-02-25 23:30:50 +0800

    text1.js

---
+++ server/formidable/test1/test1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by DaisyCream on 16/2/14.
+ */
+
+var formidable = require('formidable');
+var http = require('http');
+var util = require('util');
+
+http.createServer(function(req, res){
+    if(req.url == '' ){
+    }
+});
