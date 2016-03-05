9e8159f892de5514ea15db34813bcd49e1f832b5
DaisyCream@qq.com <DaisyCream@qq.com>
2016-03-05 11:05:43 +0800

    add the note in five

--- test1/testJS/test.js
+++ test1/testJS/test.js
@@ -39,6 +39,151 @@
 //}
 //
 //f();
+//
+//var str = ["join","haha","wah"];
+//
+//console.log(str.join("##"));
+
+//function compare(value1, value2){
+//    if(value1 < value2){
+//        return 1;
+//    }else if(value1 > value2){
+//        return -1;
+//    }else{
+//        return 0;
+//    }
+//}
+//
+//var values = [0,1,5,10,15];
+////values.sort(compare);
+//values.reverse();
+//console.log(values);
+//
+//var colors1 = ["red", "green", "blue"];
+//var colors2 = colors1.concat("yellow", ["black", "brown"]);
+//console.log(colors1);
+//console.log(colors2);
+
+
+//
+//var colors = ["red","green","blue","yellow","purple"];
+//colors.splice(1,0,"black");
+//console.log(colors);
+
+//var number = [1,2,3,4,5,4,3,2,1];
+//
+//console.log(number.indexOf(4));
+//console.log(number.lastIndexOf(4));
+
+//var person = {name : "nick"};
+//var people = [{ name : "nick"}];
+//var people1 = [person];
+//
+//console.log(people === people1);//false
+
+//
+//var number = [1,2,3,4,5,4,3,2,1];
+//
+//var everyResult = number.every(function(item, index, array){
+//    return (item > 2);
+//});
+//
+//console.log(everyResult);//false
+//
+//var someResult = number.some(function(item, index, array){
+//    return (item > 2);
+//});
+//
+//console.log(someResult);//true
+//
+//
+//
+//var filterResult = number.filter(function(item, index, array){
+//    return (item > 2);
+//});
+//
+//console.log(filterResult);
+//
+//var mapResult = number.map(function(item, index, array){
+//    return (item * 2);
+//}).join("**");
+//
+//console.log(mapResult);
+//
+//var foreachResult = number.forEach(function(item, index,array){
+//    //执行某些操作
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
