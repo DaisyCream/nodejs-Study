6bc2a72ea39e996aef48d6f168b9004fdfc7b000
DaisyCream@qq.com <DaisyCream@qq.com>
2016-03-24 15:20:14 +0800

    add the socket

--- study/socketIoStudy/test04-chat/server.js
+++ study/socketIoStudy/test04-chat/server.js
@@ -1,3 +1,26 @@
 /**
  * Created by DaisyCream on 16/3/24.
  */
+var io = require("socket.io");
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{
+        "Content-Type" :"text/html"
+    });
+    fs.readFile("./index.html","utf-8",function(err,data){
+        if(err){
+            res.end("Can't find source");
+        }
+        res.end(data);
+    })
+}).listen(8000);
+
+var socket = io.listen(server);
+
+socket.on('connection',function(client){
+    client.on("message",function(data){
+        client.send(data + " server");
+    });
+});
