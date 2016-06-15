47c46014b5b41cfe886ec5abf2b8ba6dcfb53551
DaisyCream@qq.com <DaisyCream@qq.com>
2016-06-16 00:01:48 +0800

    new life

--- studyAgain/lesson1/app.js
+++ studyAgain/lesson1/app.js
@@ -1,22 +1,18 @@
-var express = require("express");
-var http = require('http');
+/**
+ * Created by DaisyCream on 16/6/15.
+ */
+
+var express = require('express');
 
 var app = express();
-var router = express.Route();
 
-
-//use是一定有next 这个参数，其他的METHOD方法不一定有，但是因为use是中间件
-router.use(function timeLog(req, res, next){
-    console.log("Time" + Date.now());
-    next();
+app.get('/', function(req, res){
+    res.send("A");
 });
 
-router.get('/', function(req, res){
-    res.send('Add a book');
+
+app.listen(3000, function(){
+    console.log('cool');
 });
 
-router.get('/about',function(req, res){
-    res.send('About birds');
-});
 
-module.exports = router;
--- studyAgain/lesson1/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by DaisyCream on 16/6/13.
- */
-
-var router = require('');
